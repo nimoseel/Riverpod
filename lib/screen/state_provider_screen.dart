47a20ef9654b8f0c2cb3e2c5ccadd2ba09324abf
“nimoseel” <fromsomini@gmail.com>
2024-03-26 14:47:40 +0900

    Feature : NumberProvider 제작 및 적용

---
+++ lib/screen/state_provider_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:r_study/layout/default_layout.dart';
+import 'package:r_study/riverpod/state_provider_screen.dart';
+
+class StateProviderScreen extends ConsumerWidget {
+  const StateProviderScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+
+    return DefaultLayout(
+      title: 'StateProviderScreen',
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update((state) => state + 1);
+              },
+              child: Text('UP'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => _NextScreen(),
+                  ),
+                );
+              },
+              child: Text('Next Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// 똑같은 numberProvider를 watch하고 있기 때문에 동일한 숫자 값을 확인할 수 있음
+class _NextScreen extends ConsumerWidget {
+  const _NextScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(numberProvider);
+
+    return DefaultLayout(
+      title: 'StateProviderScreen',
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              provider.toString(),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(numberProvider.notifier).update((state) => state + 1);
+              },
+              child: Text('UP'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
