a42ce037dfbe92694830888af94dc6468682dbce
“nimoseel” <fromsomini@gmail.com>
2024-03-27 13:22:40 +0900

    Rename : 경로 수정

--- lib/screen/state_provider_screen.dart
+++ lib/screen/state_provider_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:r_study/layout/default_layout.dart';
-import 'package:r_study/riverpod/state_provider_screen.dart';
+import 'package:r_study/riverpod/state_provider.dart';
 
 class StateProviderScreen extends ConsumerWidget {
   const StateProviderScreen({Key? key}) : super(key: key);
