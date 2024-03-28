359f34b376f6f66202fafec38a0b7219850273ca
“nimoseel” <fromsomini@gmail.com>
2024-03-28 10:09:10 +0900

    Feature : FutureProvider 제작, FutureProviderScreen에 적용

---
+++ lib/screen/future_provider_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:r_study/layout/default_layout.dart';
+import 'package:r_study/riverpod/future_provider.dart';
+
+class FutureProviderScreen extends ConsumerWidget {
+  const FutureProviderScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AsyncValue state = ref.watch(multipleFutureProvider);
+
+    return DefaultLayout(
+      title: 'FutureProviderScreen',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          state.when(
+            data: (data) {
+              return Text(
+                data.toString(),
+                textAlign: TextAlign.center,
+              );
+            },
+            error: (err, stack) => Text(
+              err.toString(),
+            ),
+            loading: () => Center(child: CircularProgressIndicator()),
+          ),
+        ],
+      ),
+    );
+  }
+}
