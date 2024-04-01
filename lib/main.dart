e886860419c5820f0d8b9603f906dba601a06d7b
“nimoseel” <fromsomini@gmail.com>
2024-04-01 14:12:01 +0900

    Fix : observers 속성 추가하여 provider 모니터링

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:r_study/riverpod/provider_observer.dart';
 import 'package:r_study/screen/home_screen.dart';
 
 void main() {
   runApp(
     ProviderScope(
+      observers: [
+        Logger(),
+      ], // provider observer를 통해 provider 모니터링 가능
       child: MaterialApp(
         home: HomeScreen(),
       ),
