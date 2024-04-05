fbd7d4b8e1a150a300bf2767fd0027a3ceff3d34
“nimoseel” <fromsomini@gmail.com>
2024-04-05 14:30:43 +0900

    Fix : family로 파라미터 전달 받기

--- lib/riverpod/code_generation_provider.dart
+++ lib/riverpod/code_generation_provider.dart
@@ -10,12 +10,13 @@ part 'code_generation_provider.g.dart';
 final _testProvider = Provider<String>((ref) => 'Hello Code Generation');
 
 @riverpod
-String gState(GStateRef ref) { // 함수 형태로 작성하기 때문에 직관적, 코드 제너레이션을 통해 추가 기능 제공받을 수 있음
+String gState(GStateRef ref) {
+  // 함수 형태로 작성하기 때문에 직관적, 코드 제너레이션을 통해 추가 기능 제공받을 수 있음
   return 'Hello Code Generation';
 }
 
 @riverpod
-Future<int> gStateFuture(GStateFutureRef ref) async{
+Future<int> gStateFuture(GStateFutureRef ref) async {
   await Future.delayed(Duration(seconds: 3));
 
   return 10;
@@ -26,10 +27,34 @@ Future<int> gStateFuture(GStateFutureRef ref) async{
   // true값 설정시 autodispose 안걸린 provider 생성
   keepAlive: true,
 )
-Future<int> gStateFuture2(GStateFuture2Ref ref) async{
+Future<int> gStateFuture2(GStateFuture2Ref ref) async {
   await Future.delayed(Duration(seconds: 3));
 
   return 10;
 }
 
 // 2) Parameter > Family 파라미터를 일반 함수처럼 사용할 수 있도록
+class Parameter {
+  final int number1;
+  final int number2;
+
+  Parameter({
+    required this.number1,
+    required this.number2,
+  });
+}
+
+final _testFamilyProvider = Provider.family<int, Parameter>(
+  (ref, parameter) => parameter.number1 * parameter.number2,
+);
+
+// annotation은 위 코드를 아래와 같이 만들어 줄 수 있음
+// 일반함수처럼 파라미터 안에 값을 전달만 해주면 family를 사용하던 위 코드처럼 riverpod을 사용할 수 있음
+@riverpod
+int gStateMultiply(
+  GStateMultiplyRef ref, {
+  required int number1,
+  required int number2,
+}) {
+  return number1 * number2;
+}
