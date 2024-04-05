1d24023c4f4c4545169acaa3ba0650603c212b93
“nimoseel” <fromsomini@gmail.com>
2024-04-05 15:50:44 +0900

    Fix : Consumer 위젯 사용하여 변경된 state만 다시 build 되도록 하는 방법

--- lib/screen/code_generation_screen.dart
+++ lib/screen/code_generation_screen.dart
@@ -8,6 +8,10 @@ class CodeGenerationScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    print('build');
+    // 어떤 state가 변경되든지 build가 계속 다시 불리는 문제 확인 - 전체 위젯을 build
+    // 변경된 state만 다시 build되게 하고 싶은 경우
+
     final state1 = ref.watch(gStateProvider);
     final state2 = ref.watch(gStateFutureProvider);
     final state3 = ref.watch(gStateFuture2Provider);
@@ -15,7 +19,6 @@ class CodeGenerationScreen extends ConsumerWidget {
       number1: 10,
       number2: 20,
     ));
-    final state5 = ref.watch(gStateNotifierProvider);
 
     return DefaultLayout(
       title: 'CodeGenerationScreen',
@@ -48,7 +51,22 @@ class CodeGenerationScreen extends ConsumerWidget {
             loading: () => Center(child: CircularProgressIndicator()),
           ),
           Text('State4: $state4'),
-          Text('State5: $state5'),
+          Consumer(
+            // Consumer로 watch 감싸는 순간, watch 실행시 상위 빌드가 재실행되지 않고 Consumer 내의 builder 함수가 재실행
+            builder: (context, ref, child) {
+              print('Consumer build');
+              final state5 = ref.watch(gStateNotifierProvider);
+              return Row(
+                children: [
+                  Text('State5 : $state5'),
+                  child!,
+                ],
+              );
+            },
+            child: Text(' Hello'),
+            // Consumer Widget의 child 파라미터에 입력된 위젯이 child 파라미터에 그대로 제공됨.
+            // Hello라는 텍스트의 경우 Consumer 위젯 내에 있지만 매번 같이 빌드될 필요가 없기 때문에 한번만 빌드되도록 child 파라미터로 전달
+          ),
           Row(
             children: [
               ElevatedButton(
